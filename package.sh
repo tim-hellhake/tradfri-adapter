27a72ce8264946ce37a93762df3943c500aecd82
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-11-27 00:30:13 +0100

    Remove symbolic links
    https://github.com/mozilla-iot/addon-list/pull/577\#issuecomment-558859717

--- package.sh
+++ package.sh
@@ -2,6 +2,7 @@
 
 rm -rf node_modules
 npm install --production
+rm -rf node_modules/.bin
 
 shasum --algorithm 256 package.json manifest.json lib/*.js LICENSE README.md > SHA256SUMS
 find node_modules -type f -exec shasum --algorithm 256 {} \; >> SHA256SUMS
