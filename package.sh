adc95ec101b0cd72b586a89d66cd2135cd78697e
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-07-20 23:19:00 +0200

    Add cross-compile build

--- package.sh
+++ package.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+# Setup environment for building inside Dockerized toolchain
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && source "${NVM_DIR}/nvm.sh"
+[ $(id -u) = 0 ] && umask 0
+
 npm ci
 npm run build
 rm -rf node_modules
