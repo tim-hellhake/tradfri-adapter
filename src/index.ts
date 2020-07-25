61f15026c3d04d96b81704795bd389498a9f08a7
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-07-25 02:11:51 +0200

    Use all addresses for connection attempts (#9)

--- src/index.ts
+++ src/index.ts
@@ -41,68 +41,71 @@ async function discover(addonManager: any, manifest: any) {
 
     if (result) {
         console.log(`Discovered gateway ${result.name} at ${result.host}`);
-        const database = new Database(manifest.name);
-        await database.open();
-        const config = <Config>await database.loadConfig();
-        console.log(config);
+        let hosts: string[] = [];
 
-        if (!config.gateways) {
-            config.gateways = {};
+        if (result.host) {
+            hosts.push(result.host);
         }
 
-        let gatewayConfig = config.gateways[result.name];
+        let addresses = [...hosts, ...result.addresses];
 
-        if (!gatewayConfig) {
-            console.log('Adding gateway to config');
-            gatewayConfig = {};
-            config.gateways[result.name] = gatewayConfig;
-            console.log('Saving gateway');
-            await database.saveConfig(config);
-        }
-
-        const tradfri = new TradfriClient(result.host || result.addresses[0]);
-        let authentication = gatewayConfig.authentication;
-
-        if (!authentication) {
-            console.log('Authenticating to the gateway');
-
+        for (const address of addresses) {
             try {
-                authentication = await tradfri.authenticate(securityCode);
-                gatewayConfig.authentication = authentication;
+                console.log(`Trying to connect to ${address}`);
+                await connect(addonManager, manifest, result.name, address, securityCode);
+                console.log(`Connected to ${address}`);
+                return;
             } catch (e) {
                 if (e instanceof TradfriError) {
                     printTradfriError(e);
                 } else {
-                    console.log(`Could not authenticate to the gateway`);
+                    console.log(`Could not connect to the gateway: ${e}`);
                 }
-
-                return;
             }
-
-            console.log('Saving authentication');
-            await database.saveConfig(config);
-        }
-
-        const {
-            identity,
-            psk
-        } = authentication;
-
-        try {
-            await tradfri.connect(identity, psk);
-            new TradfriAdapter(addonManager, manifest, result.name, tradfri);
-        } catch (e) {
-            if (e instanceof TradfriError) {
-                printTradfriError(e);
-            } else {
-                console.log(`Could not connect to the gateway`);
-            }
-
-            return;
         }
     } else {
         console.log('No gateway found');
     }
+}
+
+async function connect(addonManager: any, manifest: any, name: string, address: string, securityCode: string) {
+    const database = new Database(manifest.name);
+    await database.open();
+    const config = <Config>await database.loadConfig();
+
+    if (!config.gateways) {
+        config.gateways = {};
+    }
+
+    let gatewayConfig = config.gateways[name];
+
+    if (!gatewayConfig) {
+        console.log('Adding gateway to config');
+        gatewayConfig = {};
+        config.gateways[name] = gatewayConfig;
+        console.log('Saving gateway');
+        await database.saveConfig(config);
+    }
+
+    const tradfri = new TradfriClient(address);
+    let authentication = gatewayConfig.authentication;
+
+    if (!authentication) {
+        console.log('Authenticating to the gateway');
+        authentication = await tradfri.authenticate(securityCode);
+        gatewayConfig.authentication = authentication;
+        console.log('Saving authentication');
+        await database.saveConfig(config);
+    }
+
+    const {
+        identity,
+        psk
+    } = authentication;
+
+    await tradfri.connect(identity, psk);
+    new TradfriAdapter(addonManager, manifest, name, tradfri);
+
 }
 
 function printTradfriError(e: TradfriError) {
